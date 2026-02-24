5c308adbc6f2806eb10ed0e95cce298e7dbad583
“Ankit <ankitchaudhary5168033@gmail.com>
2026-02-24 12:38:57 +0530

    [Ankit Chaudhary] Add Problem of UC4

---
+++ src/main/java/UtilityClasses/LengthUnit.java
@@ -0,0 +1,32 @@
+package UtilityClasses;
+
+public enum LengthUnit {
+
+    FEET {
+        public double toFeet(double value) {
+            return value;
+        }
+    },
+
+    INCH {
+        public double toFeet(double value) {
+            return value / 12.0;
+        }
+    },
+
+    YARDS {
+        public double toFeet(double value) {
+            return value * 3.0; // 1 yard = 3 feet
+        }
+    },
+
+    CENTIMETERS {
+        public double toFeet(double value) {
+            return (value * 0.393701) / 12.0;
+            // 1 cm = 0.393701 inches
+            // inches to feet divide by 12
+        }
+    };
+
+    public abstract double toFeet(double value);
+}
