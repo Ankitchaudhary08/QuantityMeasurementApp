64afd1e85d0c0041938871f98330fa9c3fd78c89
“Ankit <ankitchaudhary5168033@gmail.com>
2026-03-10 11:59:10 +0530

    [Ankit Chaudhary]Add. solve UC13

--- src/main/java/Main/QuantityMeasurementApp.java
+++ src/main/java/Main/QuantityMeasurementApp.java
@@ -6,7 +6,7 @@ public class QuantityMeasurementApp {
 
     public static <U extends IMeasurable>
     void demonstrateEquality(Quantity<U> q1, Quantity<U> q2) {
-//print
+
         System.out.println("Compare " + q1 + " and " + q2 + " -> Equal: " + q1.equals(q2));
     }
 
@@ -24,7 +24,6 @@ public class QuantityMeasurementApp {
 
     public static void main(String[] args) {
 
-        // Length Example
         Quantity<LengthUnit> l1 =
                 new Quantity<>(1.0, LengthUnit.FEET);
 
@@ -35,7 +34,7 @@ public class QuantityMeasurementApp {
         demonstrateConversion(l1, LengthUnit.INCH);
         demonstrateAddition(l1, l2, LengthUnit.FEET);
 
-        // Weight Example
+
         Quantity<WeightUnit> w1 =
                 new Quantity<>(1.0, WeightUnit.KILOGRAM);
 
@@ -52,5 +51,27 @@ public class QuantityMeasurementApp {
                 new Quantity<>(1000.0, VolumeUnit.MILLILITRE);
 
         demonstrateEquality(v1, v2);
+
+
+        Quantity<LengthUnit> l4 =
+                new Quantity<>(10.0, LengthUnit.FEET);
+        Quantity<LengthUnit> l6 =
+                new Quantity<>(6.0, LengthUnit.INCH);
+
+        System.out.println("Subtract: " + l1.subtract(l2));
+        System.out.println("Subtract (explicit inches): "
+                + l4.subtract(l6, LengthUnit.INCH));
+
+
+        System.out.println("Divide: "
+                + l1.divide(new Quantity<>(2.0, LengthUnit.FEET)));
+
+        Quantity<WeightUnit> w5 =
+                new Quantity<>(10.0, WeightUnit.KILOGRAM);
+        Quantity<WeightUnit> w8 =
+                new Quantity<>(5.0, WeightUnit.KILOGRAM);
+
+        System.out.println("Weight division: "
+                + w5.divide(w8));
     }
 }
